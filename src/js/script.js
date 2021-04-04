67fa2615c29bec56db54f90be6046c130c934a51
Lincoln <Linc.LPC.1@gmail.com>
2021-04-04 15:13:28 +0500

    Adaptive carousel

--- src/js/script.js
+++ src/js/script.js
@@ -2,6 +2,15 @@ $(document).ready(function(){
     $('.carousel__inner').slick({
         speed: 1200,
         prevArrow:'<button type="button" class="slick-prev"><img src="../img/left.svg" alt=""></button>',
-        nextArrow: '<button type="button" class="slick-next"><img src="../img/right.svg" alt=""></button>'
+        nextArrow: '<button type="button" class="slick-next"><img src="../img/right.svg" alt=""></button>',
+        responsive: [
+            {
+                breakpoint: 992,
+                settings: {
+                dots: true,
+                arrows: false
+                }
+            }
+        ]
       });
   });
